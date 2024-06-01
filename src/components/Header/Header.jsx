6327f8f40705635ce54b6beff0da08fe84d60640
saveljev27 <141144801+saveljev27@users.noreply.github.com>
2024-06-01 16:11:57 +0100

    download link

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -20,7 +20,7 @@ const Header = () => {
             knowledge and time to get the best results and solutions.
           </p>
         </div>
-        <a href="#!" className="btn">
+        <a href="/cv.pdf" download="cv" className="btn">
           Download CV
         </a>
       </div>
