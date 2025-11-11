dc57087c9a909341d63b65a657a7259d02f2782a
Olegs Saveljevs <141144801+saveljev27@users.noreply.github.com>
2025-11-11 14:34:54 +0200

    Update Header.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -10,19 +10,6 @@ const Header = () => {
           </strong>
           <br />
         </h1>
-        <h3 className="header__subtitle">I am frontend developer</h3>
-        <div className="header__text">
-          <p>
-            with a passion for my job. Motivated, initiative and fast learning
-            person. I have worked with responsive web development and have
-            gained a solid foundation for creating web applications using modern
-            JavaScript frameworks and libraries. I'm ready to invest all my
-            knowledge and time to get the best results and solutions.
-          </p>
-        </div>
-        <a href="/cv_os.pdf" download="cv" className="btn">
-          Download CV
-        </a>
       </div>
     </header>
   );
